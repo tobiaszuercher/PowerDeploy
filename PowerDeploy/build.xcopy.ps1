4e41d92478975353f5633aff160a7ebb9be776d5
Tobias Zürcher <tobias.zuercher@zuehlke.com>
2013-02-06 00:01:53 +0100

    first draft of prepare

--- PowerDeploy/build.xcopy.ps1
+++ PowerDeploy/build.xcopy.ps1
@@ -1,6 +1,6 @@
 properties {
   $projectFile = $null,
-  $configId = $null,
+  $packageId = $null,
   $outputPath = $null,
   $outputDir = $null,
   $toolsDir = $null
@@ -14,15 +14,42 @@ task Build `
   -requiredVariables projectFile `
 {   
     write-host "output: $outputDir"
-    exec { msbuild $projectFile /p:Configuration=Release /p:RunCodeAnalysis=false /p:OutputPath=$outputDir /t:Rebuild }
+    exec { msbuild $projectFile /p:Configuration=Release /p:RunCodeAnalysis=false /p:OutputPath=$outputDir/out /verbosity:minimal /t:Rebuild }
 }
 
 task Package `
   -description "Packages the build output into one single zip" `
-  -depends Build `
+  -depends Build, AddPackageParameters `
 {
     $context = Get-PowerDeployContext
     set-alias sz C:\git\PowerDeploy\PowerDeploy\tools\7Zip\7za.exe
 
-    sz a -tzip (Join-Path $context.paths.project "deployment/deploymentUnits/$($configId)_1.0.0.0.zip") "$outputDir/*"
+    sz a -tzip (Join-Path $context.paths.project "deployment/deploymentUnits/$($packageId)_1.0.0.0.zip") "$outputDir/*"
+    
+    # Remove-Item $outputDir -Recurse
+}
+
+task AddPackageParameters `
+{    
+    $file = Join-Path $outputdir "package.template.xml"
+    
+    $xml = New-Object System.Xml.XmlTextWriter($file, $null)
+    $xml.Formatting = "Indented"
+    $xml.Indentation = 4
+    
+    $xml.WriteStartDocument()
+    $xml.WriteStartElement("package")
+    $xml.WriteAttributeString("type", "xcopy")
+    $xml.WriteAttributeString("id", "Bla")
+    $xml.WriteAttributeString("version", "1.3.3.7")
+    $xml.WriteAttributeString("environment", "") # `${env + subenv}
+    
+    # pass to each individual impl:
+    $xml.WriteElementString("dbserver", "`${MOVIE_DB_Server}")
+    $xml.WriteElementString("dbname", "`${MOVIE_DB_Name}")
+    $xml.WriteEndElement()
+    $xml.WriteEndDocument()
+    
+    $xml.Flush()
+    $xml.Close()
 }
