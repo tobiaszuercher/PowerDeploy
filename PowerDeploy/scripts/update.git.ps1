fda968d3edb3654ff83db9ed7f7cb6129b9cab86
tobiaszuercher <tobias.zuercher@gmail.com>
2013-02-23 00:59:57 +0100

    updated git update :D

--- PowerDeploy/scripts/update.git.ps1
+++ PowerDeploy/scripts/update.git.ps1
@@ -1,16 +1,20 @@
 # TODO: read git-executable from project.xml
 
-if(Test-Path $powerdeploy.project.update.target -eq $false)
+if((Test-Path $powerdeploy.project.update.target) -eq $false)
 {
-	New-Item -Path $powerdeploy.project.update.target -ItemType directory
+	New-Item -Path $powerdeploy.project.update.target -ItemType directory | Out-Null
+
+	Write-Host "No PowerDeploy installation found. Install one, stay tuned!"
 
 	git clone $powerdeploy.project.update.repository $powerdeploy.project.update.target
 }
+else
+{
+	$location = Get-Location
 
-$location = Get-Location
+	Set-Location $powerdeploy.project.update.target
 
-Set-Location $powerdeploy.project.update.target
+	git pull
 
-git pull
-
-Set-Location $location
+	Set-Location $location	
+}
