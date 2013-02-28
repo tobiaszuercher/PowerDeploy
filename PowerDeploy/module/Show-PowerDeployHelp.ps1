6017ccdb2f31682fd6ebd300258a9e19c43018ff
tobiaszuercher <tobias.zuercher@gmail.com>
2013-02-28 13:31:33 +0100

    added more sample environments and added some nice coloring for help

--- PowerDeploy/module/Show-PowerDeployHelp.ps1
+++ PowerDeploy/module/Show-PowerDeployHelp.ps1
@@ -1,8 +1,12 @@
 function Show-PowerDeployHelp()
 {
 	Write-Host "The following commands are available"
-	Write-Host "  Build {unit-type}"
-	Write-Host "  Prepare {deploymentUnitSet} {environment}"
-	Write-Host "  Configure {environment}"
-	Write-Host "  Deploy {deploymentUnitSet} {environment} TODO"
+	Write-Host "  Build " -nonewline -f "White"
+	Write-Host "{unit-type}"
+	Write-Host "  Prepare " -nonewline -f "White"
+	Write-Host "{deploymentUnitSet} {environment}"
+	Write-Host "  Configure " -nonewline -f "White"
+	Write-Host "{environment}"
+	Write-Host "  Deploy " -nonewline -f "White"
+	Write-Host "{deploymentUnitSet} {environment}"
 }
