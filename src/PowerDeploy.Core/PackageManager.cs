b439a7f88ac4bf348f5d2e5e4014e26e2456c5a4
Tobias Zürcher <tobias.zuercher@gmail.com>
2013-12-24 01:59:01 +0100

    added some log statements

--- src/PowerDeploy.Core/PackageManager.cs
+++ src/PowerDeploy.Core/PackageManager.cs
@@ -7,6 +7,7 @@ using Ionic.Zip;
 
 using NuGet;
 
+using PowerDeploy.Core.Logging;
 using PowerDeploy.Core.Template;
 
 namespace PowerDeploy.Core
@@ -15,6 +16,8 @@ namespace PowerDeploy.Core
     {
         private readonly IFileSystem _fileSystem;
         private readonly TemplateEngine _templateEngine;
+
+        private readonly ILog _logger;
 
         public IEnviornmentProvider EnviornmentProvider { get; set; }
 
@@ -28,15 +31,19 @@ namespace PowerDeploy.Core
             _fileSystem = fileSystem;
             _templateEngine = new TemplateEngine(fileSystem, environmentProvider);
             EnviornmentProvider = environmentProvider;
+            _logger = LogManager.GetLogger(GetType());
         }
 
         public void ConfigurePackage(string packagePath, string environment, string outputPath)
         {
-            Console.WriteLine("Configuring package {0} for {1}", new FileInfo(packagePath).Name, environment.ToUpper());
+            _logger.InfoFormat("Configuring package {0} for {1}", new FileInfo(packagePath).Name, environment.ToUpper());
             var workingDir = _fileSystem.CreateTempWorkingDir();
+
+            _logger.DebugFormat("Create temp work dir {0}", workingDir);
 
             // read nupkg metadata
             var nupkg = new ZipPackage(packagePath);
+            _logger.DebugFormat("Unzipping {0} to {1}", nupkg.GetFullName(), workingDir);
 
             using (var zip = new ZipFile(packagePath))
             {
--- src/PowerDeploy.Core/Template/TemplateEngine.cs
+++ src/PowerDeploy.Core/Template/TemplateEngine.cs
@@ -1,10 +1,14 @@
-﻿using System.IO;
+﻿using System.Globalization;
+using System.IO;
+
+using PowerDeploy.Core.Logging;
 
 namespace PowerDeploy.Core.Template
 {
     public class TemplateEngine
     {
         private readonly IFileSystem _fileSystem;
+        private readonly ILog _logger;
 
         public IEnviornmentProvider EnviornmentProvider { get; set; }
         public VariableResolver VariableResolver { get; set; }
@@ -16,18 +20,21 @@ namespace PowerDeploy.Core.Template
 
         public TemplateEngine(IFileSystem fileSystem, IEnviornmentProvider environmentProvider)
         {
+            _logger = LogManager.GetLogger(GetType());
             _fileSystem = fileSystem;
             EnviornmentProvider = environmentProvider;
         }
 
         public void TransformDirectory(string path, string environment)
         {
+            _logger.DebugFormat("Transforming {0} for environment {1}", path, environment.ToUpper(CultureInfo.InvariantCulture));
             var targetEnvironment = EnviornmentProvider.GetVariables(environment);
 
             VariableResolver = new VariableResolver(targetEnvironment.Variables);
 
             foreach (var templateFile in _fileSystem.EnumerateDirectoryRecursive(path, "*.template.*", SearchOption.AllDirectories))
             {
+                _logger.DebugFormat("Transforming {0}", templateFile);
                 var templateText = _fileSystem.ReadFile(templateFile);
                 var transformed = VariableResolver.TransformVariables(templateText);
                 _fileSystem.OverwriteFile(templateFile.Replace(".template.", "."), transformed);
