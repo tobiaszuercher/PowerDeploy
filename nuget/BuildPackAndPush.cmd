a617e37cb272dc687d4e27cca9d6a3185521916e
Tobias Zürcher <tobias.zuercher@gmail.com>
2014-02-19 17:59:59 +0100

    and cmd file to build and publish

--- nuget/BuildPackAndPush.cmd
+++ nuget/BuildPackAndPush.cmd
@@ -4,6 +4,6 @@ DEL ..\src\PowerDeploy.PackageManagerExtension\*.nupkg
 
 REM %NUGET% pack ..\src\PowerDeploy.PackageManagerExtension\PowerDeploy.PackageManagerExtension.nuspec
 
-%MSBUILD% ..\src\PowerDeploy.sln /t:Rebuild /p:Configuration=Package /p:OctoPackPublishPackageToFileShare=C:\temp\nuget.server /p:OctoPackPackageVersion=0.0.3.18
+%MSBUILD% ..\src\PowerDeploy.sln /t:Rebuild /p:Configuration=Package /p:OctoPackPublishPackageToFileShare=C:\temp\nuget.server /p:OctoPackPackageVersion=0.0.3.19
 
 REM COPY ..\src\PowerDeploy.PackageManagerExtension\src\*.nupkg C:\temp\nuget.server
