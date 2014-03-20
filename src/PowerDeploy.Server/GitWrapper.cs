2d411c3426222859eca555075ef755d419859979
Tobias Zuercher <tobias.zuercher@gmail.com>
2014-03-20 22:58:35 +0100

    add git wrapper

---
+++ src/PowerDeploy.Server/GitWrapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+using PowerDeploy.Core;
+
+namespace PowerDeploy.Server
+{
+    public class GitWrapper
+    {
+        private readonly string _directory;
+        private string _gitExecutable;
+        private Action<string> _output;
+
+        public GitWrapper(string directory, string gitExecutable = "git.exe")
+        {
+            _directory = directory;
+            _output = Console.WriteLine;
+        }
+
+        public void Clone(string repository)
+        {
+            SilentProcessRunner.ExecuteCommand(_gitExecutable, "clone -q", repository, _output, e => _output("ERROR: " + e));
+        }
+
+        public void Pull()
+        {
+            SilentProcessRunner.ExecuteCommand(_gitExecutable, "pull -q", _directory, _output, e => _output("ERROR: " + e));
+        }
+
+        public void PullOrCloneRepository(string repository)
+        {
+            if (!Directory.Exists(Path.Combine(_directory, ".git")))
+            {
+                Clone(repository);
+            }
+        }
+    }
+}
