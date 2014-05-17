ce8f7f8f2be24e2f8df8d977c16cf55b4aabc028
Tobias Zürcher <tobias.zuercher@gmail.com>
2014-05-17 13:37:40 +0200

    splitted msbuild project into mutliple msbuild projects for each version

---
+++ src/PowerDeploy.MsBuild/Properties/AssemblyInfo.v.40.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PowerDeploy.MsBuild.v4.0")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zuehlke Engineering AG")]
+[assembly: AssemblyProduct("PowerDeploy.MsBuild")]
+[assembly: AssemblyCopyright("Copyright © Zuehlke Engineering AG 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("79fb6eae-4bd2-4acc-a931-a6f6aae37bb3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.4.4.0")]
+[assembly: AssemblyFileVersion("0.4.4.0")]
