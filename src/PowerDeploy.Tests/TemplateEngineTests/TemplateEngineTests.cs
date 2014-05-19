35dd4862ace5971c7557080ad971c30f740a67ed
Tobias Zürcher <tobias.zuercher@gmail.com>
2014-05-19 23:59:37 +0200

    add new test for readonly

--- src/PowerDeploy.Tests/TemplateEngineTests/TemplateEngineTests.cs
+++ src/PowerDeploy.Tests/TemplateEngineTests/TemplateEngineTests.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-
+using System.IO;
 using NUnit.Framework;
 
 using Moq;
@@ -21,6 +21,25 @@ namespace PowerDeploy.Tests.TemplateEngineTests
             var target = new TemplateEngine();
             ////target.ConfigurePackage(@"c:\temp\nuget\Testpackage.1.0.0.nupkg", "DEV", @"c:\temp\");
             /// // TODO:
+        }
+
+        [Test]
+        public void Transform_Read_Only_File()
+        {
+            var mock = new Mock<IEnvironmentSerializer>();
+            mock.Setup(provider => provider.Deserialize("unit")).Returns(GetUnitEnvironment());
+
+            var target = new TemplateEngine();
+            using (var dir = new TestFolder())
+            {
+                dir.AddFile("read-only.template.txt", "whatever: ${var1}");
+                var file = dir.AddFile("read-only.txt", "empty");
+
+                File.SetAttributes(Path.Combine(dir.DirectoryInfo.FullName, file), FileAttributes.ReadOnly);
+
+                target.TransformDirectory(dir.DirectoryInfo.FullName, GetUnitEnvironment(), false);
+
+            }
         }
 
         private Environment GetUnitEnvironment()
